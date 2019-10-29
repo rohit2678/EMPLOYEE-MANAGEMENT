c26540bb4529a6fe4b8e52d05a028f9c149c8350
Rohit Prasad <rohitprasad267@gmail.com>
2019-10-29 19:39:38 +0530

    saved

--- EmployeeManagement/src/app/add-employee/add-employee.component.ts
+++ EmployeeManagement/src/app/add-employee/add-employee.component.ts
@@ -14,7 +14,7 @@ export class AddEmployeeComponent implements OnInit {
   email: string;
   contact: number;
   designation: string
-  employee: Employee;
+  employee: Array<Employee> = [];
 
   constructor(private task: TaskService) { }
 
@@ -23,25 +23,35 @@ export class AddEmployeeComponent implements OnInit {
   }
 
   onAdd() {
-    this.task.employee.push({ name: this.name.toUpperCase(), avatar: ("E" + (Math.floor(100000 + Math.random() * 900000))), companyName: this.company.toUpperCase(), contactNo: this.contact, designation: this.designation.toUpperCase(), emailID: this.email.toUpperCase() });
+    //this.task.employee.push({ name: this.name.toUpperCase(), avatar: ("E" + (Math.floor(100000 + Math.random() * 900000))), companyName: this.company.toUpperCase(), contactNo: this.contact, designation: this.designation.toUpperCase(), emailID: this.email.toUpperCase() });
     const employeeInfo = localStorage.getItem("employee");
+    this.employee = [];
     if (employeeInfo) {
       const employeeArray = JSON.parse(employeeInfo);
       const employeeFound = employeeArray.find((employee: Employee) => employee.name == this.name.toUpperCase() && employee.companyName == this.company.toUpperCase() && employee.contactNo == this.contact && employee.designation == this.designation.toUpperCase());
+      const employeeEmailFound = employeeArray.find((employee: Employee) => employee.emailID == this.email.toUpperCase());
+      const employeeNumberFound = employeeArray.find((employee: Employee) => employee.contactNo == this.contact);
       if (employeeFound) {
         alert("Employee already Exists");
+      }
+      else if(employeeEmailFound){
+        alert("Email Already exists");
+      }
+      else if(employeeNumberFound){
+        alert("Mobile Number Already Exists");
       }
       else {
         console.log("EmployeeList ", employeeArray);
         console.log("Employee", this.task.employee);
-        this.task.employee.push(...employeeArray);
-
-        localStorage.setItem("employee", JSON.stringify(this.task.employee));
+        this.employee.push(...employeeArray);
+        this.employee.push({ name: this.name.toUpperCase(), avatar: ("E" + (Math.floor(100000 + Math.random() * 900000))), companyName: this.company.toUpperCase(), contactNo: this.contact, designation: this.designation.toUpperCase(), emailID: this.email.toUpperCase() });
+        localStorage.setItem("employee", JSON.stringify(this.employee));
       }
     }
     else {
       console.log("Going to else");
-      localStorage.setItem("employee", JSON.stringify(this.task.employee));
+      this.employee.push({ name: this.name.toUpperCase(), avatar: ("E" + (Math.floor(100000 + Math.random() * 900000))), companyName: this.company.toUpperCase(), contactNo: this.contact, designation: this.designation.toUpperCase(), emailID: this.email.toUpperCase() });
+      localStorage.setItem("employee", JSON.stringify(this.employee));
     }
     this.name = "";
     this.company = "";
